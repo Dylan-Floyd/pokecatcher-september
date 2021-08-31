5f4c8dfa6ebf8dd8bc848549330c35a8b31ab2ca
harriet-the-spy <dpcairns@gmail.com>
2021-08-31 10:59:35 -0700

    add random pokemon function

---
+++ utils.js
@@ -0,0 +1,26 @@
+import pokemon from './data.js';
+
+function getRandomIndex() {
+    return Math.floor(Math.random() * pokemon.length);
+}
+
+export function getRandomPokemon() {
+    let randomIndex1 = getRandomIndex();
+    let randomIndex2 = getRandomIndex();
+    let randomIndex3 = getRandomIndex();
+
+    while (
+        randomIndex1 === randomIndex2 
+        || randomIndex2 === randomIndex3 
+        || randomIndex1 === randomIndex3) {
+        randomIndex1 = getRandomIndex();
+        randomIndex2 = getRandomIndex();
+        randomIndex3 = getRandomIndex();
+    }
+
+    return [
+        pokemon[randomIndex1], 
+        pokemon[randomIndex2], 
+        pokemon[randomIndex3]
+    ];
+}
