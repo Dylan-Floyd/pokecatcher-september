2713d38d7818744aeaf7ef2a941caaa2e365bd3c
Dylan Floyd <floyd800@gmail.com>
2021-09-01 15:21:53 -0700

    add stretch charts

--- chart-utils.js
+++ chart-utils.js
@@ -32,6 +32,41 @@ export function getCatchesChartData() {
     return { labels, data };
 }
 
+/*
+ * Example return data format for (getPokedex, 'type_1', 'NA'):
+ *  {
+ *      values: ['grass', 'poison'],
+ *      counts: [1, 3]
+ *  }
+ */
+export function countValuesForKey(arrayOfObjects, key, ignoreValue) {
+    let valuesData = {
+        values: [],
+        counts: []
+    };
+    for (let currKey of Object.keys(arrayOfObjects)) {
+        const pokemonData = findPokemonById(arrayOfObjects[currKey].id);
+        const value = pokemonData[key];
+        //don't count the value it's equal to ignoreValue and
+        //do count the value if ignoreValue wasn't specified.
+        if (value === ignoreValue && typeof ignoreValue !== 'undefined') {
+            continue;
+        }
+
+        for (let i = 0; i < valuesData.values.length; i++) {
+            if (valuesData.values[i] === value) {
+                valuesData.counts[i] += 1;
+                break;
+            }
+        }
+        //If this code runs, the for loop didn't find an entry
+        //for the newType, so add it and set the count;
+        valuesData.values.push(value);
+        valuesData.counts.push(1);
+    }
+    return valuesData;
+}
+
 export function attachEncountersChart(targetCanvas) {
     let encountersData = getEncounterChartData();
     // eslint-disable-next-line no-undef
@@ -81,6 +116,84 @@ export function attachCatchesChart(targetCanvas) {
             datasets: [{
                 label: '# of Catches',
                 data: catchesData.data,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+export function attachType1Chart(targetCanvas) {
+    let type1Data = countValuesForKey(getPokedexData(), 'type_1', 'NA');
+    // eslint-disable-next-line no-undef
+    new Chart(targetCanvas, {
+        type: 'bar',
+        data: {
+            labels: type1Data.values,
+            datasets: [{
+                label: '# of Catches',
+                data: type1Data.counts,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+export function attachValueCountChart(targetCanvas, key, ignoreValue, chartLabel) {
+    let type1Data = countValuesForKey(getPokedexData(), key, ignoreValue);
+    // eslint-disable-next-line no-undef
+    new Chart(targetCanvas, {
+        type: 'bar',
+        data: {
+            labels: type1Data.values,
+            datasets: [{
+                label: chartLabel,
+                data: type1Data.counts,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
--- results/results.js
+++ results/results.js
@@ -1,4 +1,4 @@
-import { attachCatchesChart, attachEncountersChart } from '../chart-utils.js';
+import { attachCatchesChart, attachEncountersChart, attachValueCountChart } from '../chart-utils.js';
 import { getPokedexData, getTotalCaught, getTotalEncounters, storePokedexData } from '../pokedex.js';
 import { findPokemonById } from '../utils.js';
 
@@ -35,10 +35,34 @@ againButton.addEventListener('click', () => {
     window.location.href = '../';
 });
 
-//Render chart 1
+//Render charts
 const chartCanvas1 = document.querySelector('#chart-canvas-1');
 attachEncountersChart(chartCanvas1);
 
-//Render chart 2:
 const chartCanvas2 = document.querySelector('#chart-canvas-2');
 attachCatchesChart(chartCanvas2);
+
+//Render strech charts
+const chartCanvas3 = document.querySelector('#chart-canvas-3');
+attachValueCountChart(chartCanvas3, 'type_1', undefined, 'Occurrences of Type');
+
+const chartCanvas4 = document.querySelector('#chart-canvas-4');
+attachValueCountChart(chartCanvas4, 'type_2', 'NA', 'Occurrences of Secondary Type');
+
+const chartCanvas5 = document.querySelector('#chart-canvas-5');
+attachValueCountChart(chartCanvas5, 'ability_1', undefined, 'Occurrences of Main Ability');
+
+const chartCanvas6 = document.querySelector('#chart-canvas-6');
+attachValueCountChart(chartCanvas6, 'ability_2', 'NA', 'Occurrences of Secondary Ability');
+
+const chartCanvas7 = document.querySelector('#chart-canvas-7');
+attachValueCountChart(chartCanvas7, 'height', undefined, 'Occurrences of Height');
+
+const chartCanvas8 = document.querySelector('#chart-canvas-8');
+attachValueCountChart(chartCanvas8, 'weight', undefined, 'Occurrences of Weight');
+
+const chartCanvas9 = document.querySelector('#chart-canvas-9');
+attachValueCountChart(chartCanvas9, 'pokebase', undefined, 'Occurrences of Base Evolution');
+
+const chartCanvas10 = document.querySelector('#chart-canvas-10');
+attachValueCountChart(chartCanvas10, 'attack', undefined, 'Occurrences of Attack Level');
